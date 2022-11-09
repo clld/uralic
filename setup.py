8fea7a71b7a0f7f315fd76f759143c4161486cca
Robert Forkel <xrotwang@googlemail.com>
2022-11-09 11:19:34 +0100

    upgrade clld

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'clld>=9.2.1',
+        'clld>=9.2.2',
         'clld-phylogeny-plugin',
         'clld-glottologfamily-plugin>=4.0',
         'pyglottolog',
